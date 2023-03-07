b9cc3a1a0752069a14b3654c21d7dcd54f97481e
tuanakara1 <kara.tuana@std.ieu.edu.tr>
2023-03-07 11:59:08 +0300

    Initial commit

---
+++ lab1.2.py
@@ -0,0 +1,11 @@
+var1=int(input('Enter first value: '))
+var2=int(input('Enter second value: '))
+sum=var1+var2
+diff=var1-var2
+prod=var1*var2
+print('First value : ' ,var1)
+print('Second value : ' ,var2)
+print('Summation : ' ,sum)
+print('Difference: ' ,diff)
+print('Multiplication: ' ,prod)
+
---
+++ myFirstLabScript.py
@@ -0,0 +1,5 @@
+x=input('What is your name?:')
+print('Hello '+ x +'.')
+
+y=input('What is your Student ID?')
+print('Your ID is ' + y +'.')
